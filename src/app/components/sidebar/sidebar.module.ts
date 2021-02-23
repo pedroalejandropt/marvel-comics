cc33e810eafe16d2cb5a7859b3f99e3d668420ae
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-23 11:31:10 -0400

    ADD: Import Materials Modules

---
+++ src/app/components/sidebar/sidebar.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { SidebarRoutingModule } from './sidebar-routing.module';
+import { SidebarComponent } from './sidebar.component';
+
+@NgModule({
+  declarations: [
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    SidebarRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class SidebarModule { }
