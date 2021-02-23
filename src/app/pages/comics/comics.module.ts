811ede45b578c963ca220454019c3ae9c184a491
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-23 11:27:16 -0400

    ADD: Comics Component, Module && Router Module

---
+++ src/app/pages/comics/comics.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComicsRoutingModule } from './comics-routing.module';
+import { ComicsComponent } from './comics.component';
+
+
+@NgModule({
+  declarations: [
+    ComicsComponent
+  ],
+  imports: [
+    CommonModule,
+    ComicsRoutingModule
+  ]
+})
+export class ComicsModule { }
