15a4d6fe431cf053967fbb10f8be7b27a61c9db6
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-23 11:32:26 -0400

    ADD: Routes in Main Router Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: AppComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './components/sidebar/sidebar.module#SidebarModule'
+      }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
