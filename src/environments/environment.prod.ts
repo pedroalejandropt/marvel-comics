30ffacb29f3b0ba27049d86dace54f482d2e61a3
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-26 11:06:39 -0400

    ADD: Production Enviroment Variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import { Url } from 'src/assets/config/config';
+
 export const environment = {
-  production: true
+  production: true,
+  marvelAPI: Url.local,
+  apikey: 'b019ca6437266758449b0dff5a8b514b'
 };
