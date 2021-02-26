ca7f14ef496aac1e6a9b37b3bf94e0d512fef7f9
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-26 10:48:22 -0400

    ADD: CharacterComponent ADD: Filter By Name ADD: APP-GRID-RECORDS CALL ADD: APP-DETAIL-RECORD CALL ADD: Loading Spinner

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CharactersService } from 'src/app/services/characters.service';
 
+/**
+ * Component CharactersComponent
+ */
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
@@ -7,9 +11,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersComponent implements OnInit {
 
-  constructor() { }
+  character: any = null;
+  characters: any[];
+  characters_copy: any[];
+  character_text: string = '';
+
+  comics: any[];
+  series: any[];
+
+  show: boolean = true;
+
+  constructor(private _service: CharactersService) { }
 
   ngOnInit() {
+    this._service.fetchCharacters().then((res: any) => { this.characters = res.data.results; this.characters_copy = res.data.results; this.show = false; })
   }
+
+  /**
+    * This function filter the characters using the name as condition.
+  */
+  filter = () => this.characters = this.characters_copy.filter((character) => (character.name).toUpperCase().includes(this.character_text.toUpperCase()))
+  
+  /**
+    * This function clean filter input.
+  */
+  clear = () => this.characters = this.characters_copy;
+
+  /**
+    * This function get list of all the comics about the character.
+    *
+    * @param {number} id  Id of the character.
+  */
+  fetchComics = async (id: number) => await this._service.fetchComicsByCharacter(id).then(async (res: any) => { this.comics = await res.data.results; })
+  
+  /**
+    * This function get list of all the series about the character.
+    *
+    * @param {number} id  Id of the character.
+  */
+  fetchSeries = async (id: number) => await this._service.fetchSeriesByCharacter(id).then(async (res: any) => { this.series = await res.data.results; })
+
+  /**
+    * This function save all the information about the character and show the detail.
+    *
+    * @param {any} charater  Character of marvel.
+  */
+  viewDetail = async (character) => {
+    await this.fetchComics(character.id);
+    await this.fetchSeries(character.id);
+    this.character = await character;
+    this.character['slider'] = await []
+    await (this.comics.length > 0) ? this.character['slider'].push({ name: 'Comics', res: await this.comics }) : null
+    await (this.series.length > 0) ? this.character['slider'].push({ name: 'Series', res: await this.series }) : null
+  }
+
+  /**
+    * This function close the character detail.
+  */
+  closeDetail = () => this.character = null;
 
 }
--- src/app/pages/characters/characters.module.ts
+++ src/app/pages/characters/characters.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CharactersRoutingModule } from './characters-routing.module';
 import { CharactersComponent } from './characters.component';
-
+import { GridRecordsModule } from 'src/app/components/grid-records/grid-records.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { DetailRecordModule } from 'src/app/components/detail-record/detail-record.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { CharactersComponent } from './characters.component';
   ],
   imports: [
     CommonModule,
-    CharactersRoutingModule
+    CharactersRoutingModule,
+    GridRecordsModule,
+    DetailRecordModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatProgressSpinnerModule
   ]
 })
 export class CharactersModule { }
