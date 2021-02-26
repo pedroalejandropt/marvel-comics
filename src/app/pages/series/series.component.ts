f5465a80970f4ecd0a0755fcf969b0df41c7abc5
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-26 10:51:09 -0400

    ADD: SeriesComponent ADD: Filter By Name, Type && Rating ADD: APP-GRID-RECORDS CALL ADD: APP-DETAIL-RECORD CALL ADD: Loading Spinner

--- src/app/pages/series/series.component.ts
+++ src/app/pages/series/series.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SeriesService } from 'src/app/services/series.service';
 
 @Component({
   selector: 'app-series',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeriesComponent implements OnInit {
 
-  constructor() { }
+  type: string = '';
+  types: any[] = [ { name: 'All', value: '' } , { name: 'Collection', value: 'collection' }, { name: 'Limited', value: 'limited' }, { name: 'One Shot', value: 'one shot' }, { name: 'Ongoing', value: 'ongoing' }]
+
+  rate: string = '';
+  ratings: any[] = [{ name: 'All', value: '' }, { name: 'A', value: 'A' }, { name: 'T', value: 'T' }, { name: 'Rated T', value: 'RATED T' }, { name: 'T+', value: 'RATED T+' }, { name: 'PSR', value: 'PSR' }, { name: 'Marvel PSR', value: 'MARVEL PSR' }]
+
+  serie: any = null;
+  series: any[];
+  series_copy: any[];
+  serie_text: string = '';
+
+  show: boolean = true;
+
+  constructor(private _service: SeriesService) { }
 
   ngOnInit() {
+    this._service.fetchSeries().then((res: any) => { this.series = res.data.results; this.series_copy = res.data.results; this.show = false; })
   }
+
+  /**
+    * This function filter the characters using the title as condition.
+  */
+  filter = () => this.series = this.series_copy.filter((serie) => (serie.title).toUpperCase().includes(this.serie_text.toUpperCase()))
+
+  /**
+    * This function filter the characters using the type and rating as condition.
+  */
+  filterSeries = async () => {
+    await this.filter();
+    this.series = await (this.type && this.rate) ? this.series.filter((serie) => serie.type.includes(this.type) && serie.rating.toUpperCase() == this.rate) : 
+    (this.type) ? this.series.filter((serie) => serie.type.includes(this.type)) : 
+    (this.rate) ? this.series.filter((serie) => serie.rating.toUpperCase() == this.rate) : this.series
+  }
+
+  /**
+    * This function clean filter input.
+  */
+  clear = () => this.series = this.series_copy;
 
 }
--- src/app/pages/series/series.module.ts
+++ src/app/pages/series/series.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SeriesRoutingModule } from './series-routing.module';
 import { SeriesComponent } from './series.component';
-
+import { GridRecordsModule } from 'src/app/components/grid-records/grid-records.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { SeriesComponent } from './series.component';
   ],
   imports: [
     CommonModule,
-    SeriesRoutingModule
+    SeriesRoutingModule,
+    GridRecordsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    MatProgressSpinnerModule
   ]
 })
 export class SeriesModule { }
