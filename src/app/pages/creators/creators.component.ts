53a1bce69b9f6db130b67cff2e02aea2b633c0a1
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-26 10:50:10 -0400

    ADD: CreatorComponent ADD: Filter By Name ADD: APP-GRID-RECORDS CALL ADD: APP-DETAIL-RECORD CALL ADD: Loading Spinner

--- src/app/pages/creators/creators.component.ts
+++ src/app/pages/creators/creators.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CreatorsService } from 'src/app/services/creators.service';
 
+/**
+ * Component CreatorsComponent
+ */
 @Component({
   selector: 'app-creators',
   templateUrl: './creators.component.html',
@@ -7,9 +11,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatorsComponent implements OnInit {
 
-  constructor() { }
+  creator: any = null;
+  creators: any[];
+  creators_copy: any[];
+  creator_text: string = '';
 
-  ngOnInit() {
+  comics: any[];
+  series: any[];
+
+  show: boolean = true;
+
+  constructor(private _service: CreatorsService) { }
+
+  ngOnInit() { 
+    this._service.fetchCreators().then((res: any) => { this.creators = res.data.results;  this.creators_copy = res.data.results; this.show = false; })
   }
+
+  /**
+    * This function filter the characters using the fullName as condition.
+  */
+  filter = () => this.creators = this.creators_copy.filter((creator) => (creator.fullName).toUpperCase().includes(this.creator_text.toUpperCase()))
+  
+  /**
+    * This function clean filter input.
+  */
+  clear = () => this.creators = this.creators_copy;
+
+  /**
+    * This function get list of all the comics about the creator.
+    *
+    * @param {number} id  Id of the creator.
+  */
+  fetchComics = async (id) => await this._service.fetchComicsByCreator(id).then(async (res: any) => { this.comics = await res.data.results; })
+  
+  /**
+    * This function get list of all the series about the creator.
+    *
+    * @param {number} id  Id of the creator.
+  */
+  fetchSeries = async (id) => await this._service.fetchSeriesByCreator(id).then(async (res: any) => { this.series = await res.data.results; })
+  
+  /**
+    * This function save all the information about the creator and show the detail.
+    *
+    * @param {any} charater  Creator of marvel.
+  */
+  viewDetail = async (creator) => {
+    await this.fetchComics(creator.id);
+    await this.fetchSeries(creator.id);
+    this.creator = await creator;
+    this.creator['slider'] = await []
+    await (this.comics.length > 0) ? this.creator['slider'].push({ name: 'Comics', res: await this.comics }) : null
+    await (this.series.length > 0) ? this.creator['slider'].push({ name: 'Series', res: await this.series }) : null
+  }
+
+  /**
+    * This function close the creator detail.
+  */
+  closeDetail = () => this.creator = null; 
 
 }
--- src/app/pages/creators/creators.module.ts
+++ src/app/pages/creators/creators.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreatorsRoutingModule } from './creators-routing.module';
 import { CreatorsComponent } from './creators.component';
-
+import { GridRecordsModule } from 'src/app/components/grid-records/grid-records.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { DetailRecordModule } from 'src/app/components/detail-record/detail-record.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { CreatorsComponent } from './creators.component';
   ],
   imports: [
     CommonModule,
-    CreatorsRoutingModule
+    CreatorsRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    GridRecordsModule,
+    DetailRecordModule,
+    FormsModule,
+    MatProgressSpinnerModule
   ]
 })
 export class CreatorsModule { }
