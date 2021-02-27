f801ba5ac1c407eb4e3aa6e8da1dced9409b6761
Pedro Pacheco <pedro.alejandro.pt@gmail.com>
2021-02-26 21:50:10 -0400

    ADD: Responsive Cards in GridRecords

--- src/app/components/grid-records/grid-records.component.ts
+++ src/app/components/grid-records/grid-records.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-grid-records',
@@ -11,10 +11,25 @@ export class GridRecordsComponent implements OnInit {
   @Input() detail;
   @Output() viewEvent = new EventEmitter<any>();
 
+  cols = 4;
+
   constructor() { }
 
-  ngOnInit() { }
+  @HostListener("window:resize", [])
+  onResize() {
+    var width = window.innerWidth;
+    this.cols = (width > 1200) ? 4 : (width < 1200 && width > 1000) ? 3 : (width < 1000 && width > 600) ? 2 : 1;
+  }
+
+  ngOnInit() { 
+    this.screenSize();
+  }
 
   view = (record) => this.viewEvent.emit(record)
+
+  screenSize = () => {
+    var width = window.innerWidth;
+    this.cols = (width > 1200) ? 4 : (width < 1200 && width > 1000) ? 3 : (width < 1000 && width > 600) ? 2 : 1;
+  }
 
 }
